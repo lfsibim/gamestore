ba560461ec3de6209b3b9f4a9127cfbbf9acd901
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2023-12-14 15:14:58 -0300

    add model produto, atributos e getters/setters

---
+++ src/main/java/com/generation/gamestore/model/Produto.java
@@ -0,0 +1,60 @@
+package com.generation.gamestore.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome do produto é obrigatório!")
+    @Size(min = 2,max = 50, message = "O nome do produto deve ter no mínimo 2 e no máximo 50 caracteres")
+    @Column(length = 50)
+    private String nome;
+
+    @Size(max = 250, message = "A descrição de ter no máximo 250 caracteres!")
+    @Column(length = 250)
+    private String descricao;
+
+    @NotBlank(message = "O preço do produto é obrigatório!")
+    private float preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    
+}
