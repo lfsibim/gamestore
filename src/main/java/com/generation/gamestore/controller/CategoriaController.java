96d6f672609bd36a7c117e8cda76d9d8432b4bbf
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2023-12-14 13:45:57 -0300

    add categoria CRUD - buscar todas as categorias

--- src/main/java/com/generation/gamestore/controller/CategoriaController.java
+++ src/main/java/com/generation/gamestore/controller/CategoriaController.java
@@ -1,5 +1,27 @@
 package com.generation.gamestore.controller;
 
-public class CategoriaController{
-    
+import com.generation.gamestore.model.Categoria;
+import com.generation.gamestore.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> getAll(){
+        return ResponseEntity.ok(categoriaRepository.findAll());
+    }
+
 }
