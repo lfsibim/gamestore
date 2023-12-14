461c0ac102a7f07bc6d9b663ae25518c0f74d4b5
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2023-12-14 13:44:51 -0300

    add categoria repository + controller

---
+++ src/main/java/com/generation/gamestore/repository/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package com.generation.gamestore.repository;
+
+import com.generation.gamestore.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
